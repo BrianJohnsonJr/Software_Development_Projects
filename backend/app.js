f29b3a09006b52782cf040dac17a81bed83e2040
rlowell-uncc <rlowell1@uncc.edu>
2024-09-20 10:14:32 -0400

    Update index.js
    changed name to app.js and modified contents a bit

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,19 @@
 const express = require('express');
 
+// Create app
 const app = express();
+
+// Set configs
+let port = 5000;
+let host = 'localhost';
+
+
 
 // backend only -- shows backend running on port 5000 in local host.
 app.get(
     "/",
     (req, res) => {
-        res.send("hello")
+        res.send("hello");
     }
 )
 app.listen(
