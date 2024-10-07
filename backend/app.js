cce6dd5b8a8bc4d49b2798e369ff50a7711bb5d3
rlowell-uncc <123588943+rlowell-uncc@users.noreply.github.com>
2024-10-07 15:43:06 -0400

    Add routes to app.js
    Added some new routes to app.js for the backend to begin checking login info.

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const methodOverride = require('method-override');
+const userInfoModel = require('./models/userInfo');
 
 // Create app
 const app = express();
@@ -8,17 +9,49 @@ const app = express();
 let port = 5000;
 let host = 'localhost';
 
+// mount middleware
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
 
 
 // backend only -- shows backend running on port 5000 in local host.
-app.get(
-    "/",
-    (req, res) => {
-        res.send("hello");
+app.get("/", (req, res) => {
+    res.send("Default backend response.");
+});
+
+// TODO: Move routes to separate files should we have a lot of them
+app.get('/login', (req, res) => {
+    // console.log(req.query);
+    if(req.query.success === 'true') {
+        console.log("worked");
+        res.send(userInfoModel.findById(req.query.id));
+    } else if (req.query.success === 'false') {
+        console.log("worked but false");
+        res.send("Your login attempt failed.");
+    } else {
+        console.log("neither");
+        res.send(userInfoModel.get());
     }
-)
-app.listen(
-    5000,
-    () => console.log("Backend is running")
-)
+});
+
+app.post('/login', (req, res) => {
+    const matchedUser = userInfoModel.matchLogin(req.query.username, req.query.password);
+    if(!matchedUser)
+        res.redirect('/login?success=false');
+    else
+        res.redirect('/login?success=true&id=' + matchedUser.id);
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    if(!err.status) {
+        err.status = 500;
+        err.message = ("Internal Server Error");
+    }
+
+    res.status(err.status);
+    res.send(err.status + ' Error: ' + err.message);
+});
+
+app.listen(port, () => console.log("Backend is running"));
 // frontend runs automatically with react on different port. 
