31a4259ef98c2341a6645cf74041146904deff4f
wbermel3rd <willathrill@gmail.com>
2024-10-10 14:49:56 -0400

    Edit App and Index files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,25 @@
-
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './Navbar'; // Import the Navbar component
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar'; // Import the Navbar component
+import Explore from './pages/Explore'; // Import your Explore component
+import Home from './pages/Home'; // Import your Home component
+import Login from './pages/Login'; // Import your Login component
+import Following from './pages/Following'; // Import your Following component
+import Sell from './pages/Sell'; // Import your Sell component
+import Profile from './pages/Profile'; // Import your Profile component
 
 function App() {
   return (
     <div className="App">
-      <Navbar /> 
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <button href="/login" type="button">Go To Login</button>
-      </header>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/explore" element={<Explore />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/following" element={<Following />} />
+        <Route path="/sell" element={<Sell />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
     </div>
   );
 }
