df029edadacbe8d1a294bd6f3f187bfc97f323c6
Brian Johnson <bj0020538@gmail.com>
2024-10-10 19:43:50 -0400

    update index.js for routing

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './pages/Layout'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'; // Import the Register component
+import './index.css';
+// import App from './App';
 import App from './App'; // Make sure your App component is in a separate file
 import reportWebVitals from './reportWebVitals';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
