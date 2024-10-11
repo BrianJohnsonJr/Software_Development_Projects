3e8e3977d8ff6c252ffb0f958f621a825abbc8e7
wbermel3rd <willathrill@gmail.com>
2024-10-11 00:29:38 -0400

    Create Footer

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,13 @@
+// components/Footer.js
+import React from 'react';
+import '../styles/Footer.css'; // Import the footer's CSS
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>© 2024 Merchsy</p>
+    </footer>
+  );
+};
+
+export default Footer;
