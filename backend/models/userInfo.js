4712394e72ef2c8cf50a1dbe7d6b312c589674a6
wbermel3rd <willathrill@gmail.com>
2024-10-28 02:13:45 -0400

    Update userInfo to modularize methods and update schema

--- backend/models/userInfo.js
+++ backend/models/userInfo.js
@@ -1,99 +1,30 @@
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+// userInfo.js
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 // Define the user schema
-const userSchema = new Schema({
-  name: {type: String, required: [true, 'name is required']},
-  email: {type: String, required: [true, 'email is required']},
-  username: {type: String, required: [true, 'username is required']},
-  password: {type: String, required: [true, 'password is required']},
-  postIds: [Schema.Types.ObjectId],
-  addressIds: [Schema.Types.ObjectId],
-  pfp: String,
-  recentlyViewed: [Schema.Types.ObjectId],
-  bio: String,
-  followers: [Schema.Types.ObjectId],
-  following: [Schema.Types.ObjectId],
-  likedPosts: [Schema.Types.ObjectId]
-});
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: [true, 'Name is required'] },
+  email: { type: String, required: [true, 'Email is required'], unique: true },
+  username: { type: String, required: [true, 'Username is required'], unique: true },
+  password: { type: String, required: [true, 'Password is required'] },
+  profilePicture: { type: String, default: '' }, // profile picture optional
+  bio: { type: String, maxLength: 300 },
+  postIds: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Post' }],
+  addressIds: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Address' }]
+}, { timestamps: true });
 
-const userModel = mongoose.model('User', userSchema);
+// Mongoose model
+const User = mongoose.model('User', userSchema);
 
-/** 
- * @deprecated Gets the entire array of user info. Should not be used once the DB is fully implemented.
- * @returns an array of all user objects
- */
-exports.get = async () => {
-  const allUsers = await userModel.find();
-  return Array.from(allUsers);
-};
-
-/**
- *  Searches the user info array for a specific ID and returns it
- */
-exports.findById = async (id) => await userModel.findById(id);
-
-/**
- * Searches the user info array for a specific username
- * @param {string} username 
- * @returns the matched user object. Otherwise, returns undefined
- */
-exports.findByUsername = async (username) => await userModel.find({ username: username });
-
-/**
- * Accepts a username and password, and verifies if the details match a user.
- * @param {string} username 
- * @param {string} pass 
- * @returns the user who was found. Otherwise returns undefined.
- */
-exports.verifyUsernameAndPassword = async (username, pass) => {
-  const user = await findByUsername(username);
-  if(!user) return undefined;
-  
-  return new Promise(async (resolve, reject) => {
-    try {
-      const success = await bcrypt.compare(pass, user.password);
-  
-      if(success) resolve(user);
-      else resolve(undefined);
+// Helper functions
+module.exports = {
+    User,
+    getAllUsers: () => User.find(),
+    findById: (id) => User.findById(id),
+    findByUsername: (username) => User.findOne({ username }),
+    addNewUser: async (userData) => {
+        const user = new User(userData);
+        return await user.save();
     }
-    catch (err) { reject(err); }
-  });
-};
-
-/**
- * Accepts a user object and returns a signed token.
- * @param {*} user 
- * @returns 
- */
-exports.tokenizeLogin = (user) => {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const token = await jwt.sign(user, process.env.TOKEN_SECRET, { expiresIn: '1h'}); // TODO: Change token expiration
-      resolve(token);
-    }
-    catch (err) { reject(err); }
-
-  })
-}
-
-/**
- * Accepts a token and verifies if it matches a user.
- * @param {string} token
- */
-exports.verifyToken = (token) => {
-  // TODO: use the function on line 124 to check after decoding the token.
-}
-
-/**
- * Adds a new user to the database
- * @param {*} user 
- * @returns 
- */
-exports.addNewUser = async (user) => {
-  const userToAdd = new model(user);
-  await userToAdd.save(userToAdd);
-  return userToAdd.id;
 };
