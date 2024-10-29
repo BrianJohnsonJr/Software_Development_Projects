0b911d830805d051d98ea3a22ac6a1b18c63748b
rlowell-uncc <123588943+rlowell-uncc@users.noreply.github.com>
2024-10-29 17:11:59 -0400

    Add routes to postsRouter.js

--- backend/routes/postsRouter.js
+++ backend/routes/postsRouter.js
@@ -1,6 +1,40 @@
 const express = require('express');
-const userInfoModel = require('../models/users');
+const User = require('../models/users');
+const Post = require('../models/posts');
 
 const router = express.Router();
+
+router.post('/create', async (req, res, next) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password'); // grab user w/o password'
+        
+        if(!user) {
+            res.status(404);
+            next(new Error('No user logged in'));
+        }
+
+        const { title, description, price, image, tags, itemType, sizes } = req.body;
+        const newPost = new Post({
+            title, 
+            description,
+            price,
+            image,
+            tags,
+            itemType,
+            sizes
+        })
+    }
+    catch (error) { next(error); }
+});
+
+router.get('/following', async (req, res, next) => {
+
+});
+
+router.get('/explore', async (req, res, next) => {
+
+});
+
+
 
 module.exports = router;
