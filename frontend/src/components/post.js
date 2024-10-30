1a3e8de9ceb38112d16cf00e07ad02f12ff57e07
Brian Johnson <bj0020538@gmail.com>
2024-10-30 13:31:12 -0400

    Add Like Button UI
    Like Button UI has been added to allow users to interact with posts.

--- frontend/src/components/post.js
+++ frontend/src/components/post.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../styles/Post.css';  
+import Likes from './Likes';
 
 function Post({ title, description, ownerUsername, price, imageUrl, tags = [], likes }) {
   return (
@@ -23,6 +24,9 @@ function Post({ title, description, ownerUsername, price, imageUrl, tags = [], l
             <li>No tags available</li>
           )}
         </ul>
+      </div>
+      <div className="post-likes">
+        <Likes/>
       </div>
     </div>
   );
