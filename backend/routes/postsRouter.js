2201da3e383500ad1b6a6f61d4c732acbf9f451e
rlowell-uncc <123588943+rlowell-uncc@users.noreply.github.com>
2024-10-30 17:04:28 -0400

    Add routing for create and explore in postsRouter.js

--- backend/routes/postsRouter.js
+++ backend/routes/postsRouter.js
@@ -27,15 +27,38 @@ router.post('/create', async (req, res, next) => {
         await newPost.save();
         res.json({ success: true, message: 'Post successfully posted' });
     }
-    catch (error) { next(error); }
+    catch (error) {
+        if(error.name === 'ValidationError')
+            error.status = 400;
+
+        next(error);
+    }
 });
 
 router.get('/following', async (req, res, next) => {
-
+    try {
+        const user = await User.findById(req.user.id, { password: 0 });
+        if(!user.following || user.following.length === 0) {
+            res.json([]);
+        } else {
+            const following = user.following;
+            // grab 25 posts, sorted by time (id) desc. 25 newest posts.
+            const posts = await Post.find({ following: {$in: following }}, null, { limit: 25, sort: {_id: -1} }); 
+            res.json(posts);
+        }
+    }
+    catch (error) { next(error); }
 });
 
 router.get('/explore', async (req, res, next) => {
-
+    try {
+        // grab 25 posts, sorted by time (id) desc. 25 newest posts.
+        // unsure which is better...
+        const posts = await Post.find({}, null, { limit: 25, sort: {_id: -1} }); 
+        // const posts = await Post.find().limit(25).sort({ _id: -1 }); 
+        res.json(posts);
+    }
+    catch (error) { next(error); }
 });
 
 
