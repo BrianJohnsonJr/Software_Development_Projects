6a98c7226b149c435d49110466efbe1bbc2a5a0d
rlowell-uncc <123588943+rlowell-uncc@users.noreply.github.com>
2024-11-19 23:04:11 -0500

    Add user search route

--- backend/routes/accountRouter.js
+++ backend/routes/accountRouter.js
@@ -8,6 +8,30 @@ const router = express.Router();
 // Route to check if the user is authenticated
 router.get('/auth-check', AuthorizeUser, controller.authCheck);
 
+/**
+ * Queries users and returns users matching the specified query.
+ * Does not allow for paging with lastId=<id>
+ */
+router.get('/search', async (req, res, next) => {
+    try {
+        const searchParams = req.query.query?.trim() || '';
+        const searchQuery = searchParams ? {
+            $or: [
+                { username: { $regex: searchParams, $options: 'i' }},
+                { name: { $regex: searchParams, $options: 'i' }},
+                { bio: { $elemMatch: { $regex: searchParams, $options: 'i' }}}
+            ],
+        }
+        : {};
+
+        const usersFound = await User.find(searchQuery).select('-password').sort({ _id: -1 }).limit(25);
+        const totalFound = await User.countDocuments(searchQuery); // Count the amount of results
+
+        res.json({ success: true, users: usersFound, resultCount: totalFound });
+    }
+    catch (err) { next(err); }
+});
+
 // Register route
 router.post('/register', uploadToMemory.none(), controller.register);
 
